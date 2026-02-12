25f352aa7d9cdc8f14f9041ca7102bf62c883901
Gabriel <--global>
2026-02-12 17:20:18 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("********************************");
+        System.out.println("Welcome to the Password Generator");
+
+        System.out.println("Password length:: ");
+        Scanner sc = new Scanner(System.in);
+
+        int passwordLenght = sc.nextInt();
+        Random random = new Random();
+
+        Password password = new Password(passwordLenght);
+
+        System.out.println(password.generatePwd());
+
+
+sc.close();
+        }
+    }
---
+++ src/Password.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Password {
+    private String CHARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%¨&*()_";
+    private int pwdLenght;
+private String pwd = "";
+
+    public Password(int pwdLenght) {
+        this.pwdLenght = pwdLenght;
+    }
+    public Password() {};
+
+    public void setPwdLenght(int pwdLenght){
+        this.pwdLenght = pwdLenght;
+    }
+    public int getPwdLenght(){
+        return this.pwdLenght;
+    }
+
+    public String generatePwd(){
+        Random rdm = new Random();
+
+        for (int i = 0; i < pwdLenght; i++){
+            char tmp = CHARACTERES.charAt(rdm.nextInt(pwdLenght));
+
+            pwd.concat(String.valueOf(tmp));
+
+
+        }
+        return pwd;
+    }
+
+}
